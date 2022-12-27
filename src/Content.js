54e052f1129ecec5f2c9c3a8dc492ef31de7b839
beniamin <avr.beniamin@icloud.com>
2022-12-28 00:30:25 +0100

    added groceries list

--- src/Content.js
+++ src/Content.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { useState } from 'react';
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
+    const [items, setItems] = useState([
+        {
+            id: 1,
+            checked: false,
+            item: "Pizza"
+        },
+        {
+            id: 2,
+            checked: false,
+            item: "Coca Cola"
+        },
+        {
+            id: 3,
+            checked: false,
+            item: "Bread"
+        }
+    ]);
+
+
   return (
     <main>
-        <p>Content</p>
+       <ul>
+            {items.map((item) => (
+                <li className='item' key={item.id}>
+                    <input
+                        type="checkbox"
+                        checked={item.checked}
+                    />
+                    <label>{item.item}</label>
+                    <FaTrashAlt role="button" tabIndex="0"/>
+                </li>
+            ))}
+       </ul>
     </main>
   )
 }
