9abbda1e7318646ed8b8770c75467c5502e7e36e
beniamin <avr.beniamin@icloud.com>
2022-12-28 01:11:03 +0100

    added empty lsit message

--- src/Content.js
+++ src/Content.js
@@ -35,21 +35,25 @@ const Content = () => {
 
   return (
     <main>
-       <ul>
-            {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <input
-                        type="checkbox"
-                        onChange={() => handleCheck(item.id)}
-                        checked={item.checked}
-                    />
-                    <label
-                        style={(item.checked) ? {textDecoration: "line-through"} : null}
-                    >{item.item}</label>
-                    <FaTrashAlt role="button" tabIndex="0" onClick={() => handleDelete(item.id)}/>
-                </li>
-            ))}
-       </ul>
+        {items.length ? (
+            <ul>
+                    {items.map((item) => (
+                        <li className='item' key={item.id}>
+                            <input
+                                type="checkbox"
+                                onChange={() => handleCheck(item.id)}
+                                checked={item.checked}
+                            />
+                            <label
+                                style={(item.checked) ? {textDecoration: "line-through"} : null}
+                            >{item.item}</label>
+                            <FaTrashAlt role="button" tabIndex="0" onClick={() => handleDelete(item.id)}/>
+                        </li>
+                    ))}
+            </ul>
+        ) : (
+            <p style={{marginTop: "2rem"}}> Your list is empty</p>
+        )}
     </main>
   )
 }
