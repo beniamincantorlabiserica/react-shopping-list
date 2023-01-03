74fc8ed7dca54a171f83c4b651cc506bb23a81eb
beniamin <avr.beniamin@icloud.com>
2023-01-03 01:49:55 +0100

    added add item functionality

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,14 +1,16 @@
 import { FaPlus } from "react-icons/fa"
 
-const AddItem = () => {
+const AddItem = ({ newItem, setNewItem, handleSubmit}) => {
   return (
-    <form className='addForm'>
+    <form className='addForm' onSubmit={handleSubmit}>
         <input
             autoFocus
             id="addItem"
             type="text"
             placeholder="Add Item"
             required
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
         />
         <button
             type='submit'
--- src/App.js
+++ src/App.js
@@ -6,40 +6,47 @@ import AddItem from "./AddItem";
 
 
 function App() {
-  const [items, setItems] = useState([
-    {
-        id: 1,
-        checked: false,
-        item: "Pizza"
-    },
-    {
-        id: 2,
-        checked: true,
-        item: "Coca Cola"
-    },
-    {
-        id: 3,
-        checked: false,
-        item: "Bread"
-    }
-]);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppinglist')));
+
+const [newItem, setNewItem] = useState('');
+
+const setAndSaveItems = (newItems) => {
+  setItems(newItems);
+  localStorage.setItem("shoppinglist", JSON.stringify(newItems));
+}
+
+const addItem = (item) => {
+  const id = items.length ? [items.lenght - 1].id + 1 : 1;
+  const myNewItem =  {id, checked: false, item}; 
+  const listItems = [...items, myNewItem];
+  setAndSaveItems(listItems);
+}
 
 const handleCheck = (id) => {
   const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked} : item);
-  setItems(listItems);
-  localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  setAndSaveItems(listItems);
 }
 
 const handleDelete = (id) => {
   const listItems = items.filter((item) => item.id !== id);
-  setItems(listItems);
-  localStorage.setItem("shoppinglist", JSON.stringify(listItems));
+  setAndSaveItems(listItems);
+}
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  if(!newItem) return;
+  addItem(newItem);
+  setNewItem('');
 }
 
   return (
     <div className="App">
           <Header title="Groceries List"/>
-          <AddItem/>
+          <AddItem
+            newItem={newItem}
+            setNewItem={setNewItem}
+            handleSubmit={handleSubmit}
+          /> 
           <Content 
             items={items}
             handleCheck={handleCheck}
